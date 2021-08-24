eb3646c78b3565e695f5f4dd460162bc4d19c5f2
Victor Mororo <victormororo@gmail.com>
2021-08-24 17:15:59 -0300

    rust: reading the distance_matrix file

--- rust/main.rs
+++ rust/main.rs
@@ -1,6 +1,17 @@
 mod Data;
 
 fn main() {
-    Data::opa();
-    
+    let mut dimension : usize = 0;
+    let mut c : Vec<Vec<f64>> = vec![vec![];0];
+
+    Data::load(&mut dimension, &mut c);
+    println!("{} \n", dimension);
+
+    for i in 0..dimension {
+        for j in 0..dimension {
+            print!("{} ", c[i][j]);
+        }
+        println!();
+    }
+
 }
