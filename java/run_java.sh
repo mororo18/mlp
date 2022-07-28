599fb5ea478a8d46f2da56bf6d19e57c9160b256
Victor Mororó <victormororo@gmail.com>
2022-07-27 21:20:00 -0300

    java: update

--- java/run_java.sh
+++ java/run_java.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
-java Main
+java -agentpath:/usr/share/visualvm/profiler/lib/deployed/jdk16/linux-amd64/libprofilerinterface.so=/usr/share/visualvm/profiler/lib,5140 Main
 # rm *.class
