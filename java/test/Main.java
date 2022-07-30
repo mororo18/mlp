54dc92eed8a6ea15068fbac98253d7c96d334b08
Victor Mororo <victormororo@gmail.com>
2022-07-30 01:26:16 -0300

    java: test update

--- java/test/Main.java
+++ java/test/Main.java
@@ -1,35 +1,59 @@
 import java.util.Random;
 
-class Main {
-    public static void main(String args[]) {
-        int size = 100;
 
-        int [][] original = new int [size][size];
+class Main {
+    private int line_size;
+    private static boolean linear = false;
+
+    private static int to_1D(int i, int j, int k, int size) {
+        return 3*(size * i + j) + k;
+    }
+
+    public static void main(String args[]) {
+        int size = 5000;
+        int a = size;
+
+        int z_size = 3;
+
+        int [][][] nested = new int [size][size][z_size];
+        int [][] partNested = new int [size][size*z_size];
+        int [] flat = new int [size*size*3]; 
         MatrixTest test = new MatrixTest(size, size);
 
-        int [] indexInt = new int [size];
-        long [] index = new long [size];
+        int [] index = new int [size];
         Random rand = new Random();
 
-      //for (int i = 0; i < size; i++) {
-      //    indexInt[i] = rand.nextInt(size);
-      //    index[i] = indexInt[i];
-      //}
+        for (int i = 0; i < size; i++) {
+            index[i] = rand.nextInt(size);
+        }
+
+        System.out.print("Teste com acesso ");
+        if (linear) System.out.println("LINEAR");
+        else System.out.println("ALEATORIO");
 
         int value;
 
+        /*====================================================*/
+
         long time = -System.nanoTime();
-        for (int i = 0; i < size; i++) {
-            for (int j = i+1; j < size; j++) {
-                //value = original[i][j];
-                value = original[indexInt[i]][indexInt[j]];
+        for (int i = 0; i < a; i++) {
+            for (int j = i+1; j < a; j++) {
+                for (int k = 0; k < z_size; k++) {
+                    if (linear) value = nested[i][j][k];
+                    else value = nested[index[i]][index[j]][k];
+                }
             }
         }
         time += System.nanoTime();
 
-        System.out.print("original\t");
-        System.out.println(time);
+        System.out.print("nested\t\t");
+        System.out.println(time/ 10e8);
 
+
+        /*====================================================*/
+
+
+        /*
         time = - System.nanoTime();
         for (int i = 0; i < size; i++) {
             for (int j = i+1; j < size; j++) {
@@ -38,9 +62,43 @@ class Main {
             }
         }
         time += System.nanoTime();
-        System.out.print("meu\t\t");
+        System.out.print("MatrixTest\t");
         System.out.println(time);
-        System.out.println(int[].class);
+        */
+
+        /*====================================================*/
+
+        time = -System.nanoTime();
+        for (int i = 0; i < size; i++) {
+            for (int j = i+1; j < size; j++) {
+                for (int k = 0; k < z_size; k++) {
+                    if (linear) value = flat[to_1D(i, j, k, size)];
+                    else value = flat[to_1D(index[i], index[j], k, size)];
+                }
+            }
+        }
+        time += System.nanoTime();
+
+        System.out.print("Flat\t\t");
+        System.out.println(time/ 10e8);
+
+        /*====================================================*/
+
+        time = -System.nanoTime();
+        for (int i = 0; i < size; i++) {
+            for (int j = i+1; j < size; j++) {
+                for (int k = 0; k < z_size; k++) {
+                    if (linear) value = partNested[i][j*z_size + k];
+                    else value = partNested[index[i]][index[j]*z_size + k];
+                }
+            }
+        }
+        time += System.nanoTime();
+
+        System.out.print("Part. Nested\t");
+        System.out.println(time/ 10e8);
+
+
 
         /*
         for (int i = 0; i < size; i++) {
