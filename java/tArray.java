e9eb3d928cb50dc0366acae3e5ef6b8b3a6a932a
Victor Mororo <victormororo@gmail.com>
2022-08-10 04:49:55 -0300

    java: buggit status

--- java/tArray.java
+++ java/tArray.java
@@ -4,8 +4,11 @@ import  java.lang.System;
 class tArray {
 
     private int [] arr;
-    private int size;
+    private int [] arrBuffer;
     private int sizeCrnt;
+    private int sizeBuffer;
+
+    private int size;
 
     interface Comparator {
         public int evaluate(int i, int j);
@@ -17,15 +20,20 @@ class tArray {
     }
 
     tArray(int []arr) {
-        this.arr = arr.clone();
-        sizeCrnt = arr.length;
-        size = arr.length;
+        this.arr        = arr.clone();
+        this.sizeCrnt   = arr.length;
+        this.size       = arr.length;
+
+        this.arrBuffer  = new int[3];
+        this.sizeBuffer = 0;
     }
 
     tArray(int size) {
-        arr = new int[size];
-        sizeCrnt = size;
-        size = arr.length;
+        this.arr        = new int[size];
+        this.arrBuffer  = new int[3];
+        this.sizeCrnt   = size;
+        this.sizeBuffer = 0;
+        this.size       = arr.length;
     }
 
     public int size() {return sizeCrnt;}
@@ -43,8 +51,10 @@ class tArray {
     }
     public void reserve(int sz) {
         this.arr = new int[sz];
+        this.arrBuffer = new int[3];
         this.size = sz;
         this.sizeCrnt = 0;
+        this.sizeBuffer = 0;
     }
 
     public tArray getArrayCopy() {
@@ -87,21 +97,24 @@ class tArray {
     }
 
     public void reinsert(int i, int j, int pos) {
-        int size = j-i+1;
-        int [] seq = new int[size];
+        this.sizeBuffer = j-i+1;
 
-        System.arraycopy(this.arr, i, seq, 0, size);
+        System.arraycopy(this.arr, i, this.arrBuffer, 0, this.sizeBuffer);
 
         if (pos < i) {
             int sz = i-pos;
             shift(pos, j+1-sz, sz);
-            System.arraycopy(seq, 0, arr, pos, size);
+            System.arraycopy(this.arrBuffer, 0, arr, pos, this.sizeBuffer);
         } else {
             int sz = pos-j-1;
             shift(j+1, i, sz);
-            System.arraycopy(seq, 0, arr, i+sz, size);
+            System.arraycopy(this.arrBuffer, 0, arr, i+sz, this.sizeBuffer);
         }
         
+        if (feasibility() != true) {
+            System.out.println("infisibu");
+            System.exit(0);
+        }
     }
 
     public void remove(int index) {
@@ -119,6 +132,18 @@ class tArray {
 
     public int get(int i) {
         return arr[i];
+    }
+
+    public boolean feasibility() {
+        boolean [] check = new boolean[this.sizeCrnt];
+        for (int i = 0; i < sizeCrnt; i++) {
+            check[this.arr[i]] = true;
+        }
+
+        for (int i = 0; i < sizeCrnt; i++) {
+            if (!check[i]) return false;
+        }
+         return true;
     }
 
 
