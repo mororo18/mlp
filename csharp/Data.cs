f57a675b33c380aea63b8128931820fa67f0fae2
Rafael Sobral de Morais <rafaelsobralm@gmail.com>
2024-12-22 12:37:53 -0300

    c# main: desmembrando classes tInfo, Data e Neighborhood

--- csharp/Data.cs
+++ csharp/Data.cs
@@ -6,9 +6,12 @@ namespace MLP {
         private int dimension;
         private double [,] matrix;
         private int [] rnd;
+        private static double [] c;
+        private int rnd_index;
 
         public Data(){
             dimension = 0;
+            rnd_index = 0;
         }
 
         public void loadData(){
@@ -42,24 +45,34 @@ namespace MLP {
             for (int i = 0; i < rnd_size; i++) {
                 rnd[i] = Int32.Parse(file[file_line++]);
             }
-            /*
-            for(int i = 0; i < dimension; i++){
-                for(int j = i+1; j < dimension; j++){
-                    Console.Write(matrix[i, j] + " ");
+
+            c = new double [dimension*dimension];
+            for (int i = 0; i < dimension; i++) {
+                for (int j = i; j < dimension; j++) {
+                    c[to_1D(i,j)]  = getDistance(i,j);
+                    c[to_1D(j,i)]  = getDistance(j,i);
                 }
-                Console.WriteLine();
             }
-            */
+        
+        }
+
+        private int to_1D(int i, int j) {
+            return dimension * i + j;
         }
 
         public double getDistance(int i, int j) {
             return matrix[i, j];
         }
 
-        public int getDimension() {
-            return dimension;
-        }
-
         public int [] GetRnd() {return rnd;}
+
+        public int GetDimen() {return dimension;}
+        public double GetCost(int i, int j) {
+            //var f = ref c[0];
+            //double ret = Unsafe.Add(ref c[0], to_1D(i, j));
+            return c[to_1D(i,j)];
+        }
+        
+        public int GetRndCrnt() {return rnd[rnd_index++];}
     }
 }
--- csharp/tInfo.cs
+++ csharp/tInfo.cs
@@ -2,53 +2,21 @@
 //using System.Runtime.CompilerServices;
 
 namespace MLP {
-    class tInfo {
-        private static double [] c;
-        //private static double [][] c;
-        private int [] rnd;
-        private int rnd_index;
-
-        private int dimension;
-
+    class tInfoIndex {
+       
         public const int T = 0;
         public const int C = 1;
         public const int W = 2;
+     
+    }
 
+    class Neighborhood{
         public const int SWAP          = 0;
         public const int REINSERTION   = 1;
         public const int OR_OPT2       = 2;
         public const int OR_OPT3       = 3;
         public const int TWO_OPT       = 4;
 
-        public const double EPSILON = 1e-15;
-
-        private int to_1D(int i, int j) {
-            return dimension * i + j;
-        }
-
-        public tInfo(int dimen, double [][] cost, int [] rnd_arr) {
-            rnd = rnd_arr;
-            rnd_index = 0;
-            dimension = dimen;
-            //c = cost;
-            c = new double [dimen*dimen];
-            for (int i = 0; i < dimen; i++) {
-                for (int j = i; j < dimen; j++) {
-                    c[to_1D(i,j)]  = cost[i][j];
-                    c[to_1D(j,i)]  = cost[i][j];
-                }
-            }
-        }
-
-        public int GetDimen() {return dimension;}
-        public double GetCost(int i, int j) {
-            //var f = ref c[0];
-            //double ret = Unsafe.Add(ref c[0], to_1D(i, j));
-            return c[to_1D(i,j)];
-        }
-        //public double GetCost(int i, int j) {return c[i][j];}
-        public int GetRndCrnt() {return rnd[rnd_index++];}
-        
     }
 
     
