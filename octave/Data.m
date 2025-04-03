1d910676ead66adc25c67e38667f9684f034549c
Victor Mororó <victormororo@gmail.com>
2025-04-03 18:37:59 -0300

    matlab: rm comments

--- octave/Data.m
+++ octave/Data.m
@@ -1,7 +1,6 @@
 function [dim, cost, ret_rnd] = Data
     txt = fileread("../distance_matrix");
     lines = strsplit(txt, "\n");
-    %typeinfo(lines);
     c = [];
     dimension = NaN;
     i = 0;
@@ -9,7 +8,6 @@ function [dim, cost, ret_rnd] = Data
     index = 1;
     dimension = mat2str(str2double(lines{index}(:)));
     dimension = str2double(dimension);
-    %dimension = str2double(strrep(substr(dimension, 2, numel(dimension)-2), ';', ''));
 
     index = index + 1;
 
@@ -21,26 +19,17 @@ function [dim, cost, ret_rnd] = Data
         str = lines(index);
         str = str{1};
         class(str);
-        %str = str(:);
         for j = i+1:dimension
-            %lines(index){1};
-            %typeinfo(str);
             class(str);
             pos = strfind(str, " ");
             if (ismatrix(pos) && isempty(pos))
-                %"yes"
                 break;
             end
             pos = pos(1, 1);
             v = mat2str(str2double(str(1:pos-1)));
             
             a = size(v);
-            %a(1,2)
-            %if (a(1,2) > 1)
-            %    v = str2double(strrep(substr(v, 2, numel(v)-2), ';', ''));
-            %else
-                v = str2double(v);
-            %end
+            v = str2double(v);
             c(i, j) = v;
             c(j, i) = v;
             class(v);
@@ -51,67 +40,15 @@ function [dim, cost, ret_rnd] = Data
     
     index = index + 2;
     rnd_size = str2double(mat2str(str2double(lines{index}(:))));
-    %rnd_size = str2double(strrep(substr(rnd_size, 2, numel(rnd_size)-2), ';', ''));
 
     rnd = [];
     for i = 1:rnd_size
         v = mat2str(str2double(lines{index + i}(:)));
         lines{index + i};
         class(v);
-        %if (numel(v) > 1)
-        %    v = str2double(strrep(substr(v, 2, numel(v)-2), ';', ''));
-        %else 
-            v = str2double(v);
-        %end
+        v = str2double(v);
         rnd(i) = v;
     end
-    
-
-   %typeinfo(lines)
-   %typeinfo(lines{2})
-   %for line = lines
-   %    str = line{1}(:);
-   %    typeinfo(str);
-   %    if (!isempty(strfind(str, "EOF")))
-   %        line;
-   %        break
-   %    end
-
-   %    if (i == 0)
-   %        v = mat2str(str2double(str));
-   %        v = str2double(strrep(substr(v, 2, numel(v)-2), ';', ''));
-   %        dimension = v;
-   %        c = zeros(dimension, dimension);
-   %        i = i + 1;
-   %        continue;
-   %    end
-
-   %    j = i + 1;
-   %    while (true)
-
-   %        pos = strfind(str, " ");
-   %        if (ismatrix(pos) && isempty(pos))
-   %            %"yes"
-   %            break;
-   %        end
-   %        pos = pos(1, 1);
-   %        v = mat2str(str2double(str(1:pos-1)));
-
-   %        if (size(v)(1,2) > 1)
-   %            v = str2double(strrep(substr(v, 2, numel(v)-2), ';', ''));
-   %        else
-   %            v = str2double(v);
-   %        end
-   %        c(i, j) = v;
-   %        c(j, i) = v;
-   %        typeinfo(v);
-   %        str = str(pos+1:end);
-   %        %break;
-   %        j = j + 1;
-   %    end
-
-   %    i = i + 1;
-   %end
 
     dim = dimension;
     cost = c;
