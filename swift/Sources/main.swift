e446b481e9bf7933997a3ac90ec45ffaed07d304
PetrusGeorge <petrus.george.leal@gmail.com>
2026-07-25 16:43:42 -0300

    Add swift to project

---
+++ swift/Sources/main.swift
@@ -0,0 +1,12 @@
+var data = Data()
+
+let clock = ContinuousClock()
+
+let iIls = data.dimension < 100 ? data.dimension : 100
+
+let time = clock.measure {
+	gils_rvnd(iMax: 10, iIls: iIls, data: &data)
+}
+let timeInSeconds = Double(time.components.seconds) + Double(time.components.attoseconds) / 1e18
+
+print("TIME:", String(format: "%.6f", timeInSeconds))
